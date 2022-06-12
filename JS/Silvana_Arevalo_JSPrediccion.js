4e75a1574c4d8295f44f1fceed7e976f3834a1f6
SilviAC04 <silvanaarevalo18@hotmail.com>
2022-06-12 17:54:40 -0500

    Subir archivo js

---
+++ JS/Silvana_Arevalo_JSPrediccion.js
@@ -0,0 +1,26 @@
+function myBirthYearFunc() {    
+    console.log("Nací en " + 1980);
+    //Naci en 1980
+}
+
+function myBirthYearFunc(EntradaAñoNacimiento) {    
+    console.log("Nací en " + EntradaAñoNacimiento);
+    //Naci en 1980function add(num1, num2){
+
+
+
+    function add(num1, num2) {    
+        console.log("¡Sumando números!");    
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);    
+        var sum = num1 + num2;    
+        console.log(sum);
+    }
+
+
+
+
+    //¡Sumando números!
+    //num1 is:num1
+    //num2 is:num2
+    //num1 + num2
